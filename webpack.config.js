ff2cdb7e4639cdd1fc7e4e59aa78c36a0090d9bd
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-26 22:49:53 +0630

    Fix logo image error by installing required package but still show errors while reloading

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
     ],
   },
 };
