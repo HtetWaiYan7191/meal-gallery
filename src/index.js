eddd48c8840bc47fc592caa60e6c1785684555b2
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-26 11:37:21 +0630

    Add fetchMeal function to get data from meal api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './style.css';
+
+const baseMealUrl = 'https://www.themealdb.com/api/json/v1/1';
+const mealCardContainer = document.querySelector('.meal-card-container');
+let meals = [];
+
+const getTotalMeal = async () => {
+  const result = await fetch(`${baseMealUrl}/filter.php?a=Canadian`);
+  const { meals } = await result.json();
+  return meals;
+};
+
+meals = await getTotalMeal();
+
+const createMealCard = async (meal) => {
+  mealCardContainer.innerHTML += `
+    <div class="meal-card" data-id = "${meal.idMeal}">
+    <figure class="">
+        <img src="${meal.strMealThumb}" alt="meal-image" class="meal-images">
+    </figure>
+    <figcaption class="d-flex justify-content-between">
+        <h2 class="meal-title">${meal.strMeal}</h2>
+        <div class="reaction-container d-flex flex-column">
+            <i class="fa-regular fa-heart"></i>
+            <span>5 likes</span>
+        </div>
+    </figcaption>
+    <div class="button-container d-flex flex-column justify-content-around">
+        <button>Comments</button>   
+        <button>Reservations</button>
+    </div>
+</div>`;
+};
+
+const fetchMeal = async () => {
+  meals.forEach((meal) => createMealCard(meal));
+};
+
+await fetchMeal();
