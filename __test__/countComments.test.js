9c1dd64faffe73709fee6e48d1f45ee36f2c7080
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-27 20:28:28 +0630

    Fix linter errors

--- __test__/countComments.test.js
+++ __test__/countComments.test.js
@@ -2,16 +2,14 @@ import { JSDOM } from 'jsdom';
 import countComments from '../src/modules/countComments';
 
 describe('countTotalMeals', () => {
-const dom = new JSDOM();
-global.document = dom.window.document;
-    test('countTotalComments updates the commentTitle text content correctly', () => {
+  const dom = new JSDOM();
+  global.document = dom.window.document;
+  test('countTotalComments updates the commentTitle text content correctly', () => {
+    const comment = ['Meal 1', 'Meal 2', 'Meal 3'];
+    const commentTitle = document.createElement('h3');
 
-        const comment = ['Meal 1', 'Meal 2', 'Meal 3'];
-        const commentTitle = document.createElement('h3');
+    countComments(commentTitle, comment);
 
-        countComments(commentTitle, comment);
-
-        expect(commentTitle.textContent).toEqual(`Comments (${comment.length})`);
-      });
-
-})
+    expect(commentTitle.textContent).toEqual(`Comments (${comment.length})`);
+  });
+});
